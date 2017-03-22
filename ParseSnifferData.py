c344a9bf62e459bc6ae7cedc6fa6e9a2cfa49696
DfAC <DfAC@users.noreply.github.com>
2017-03-22 22:56:22 +0000

    python script works properly with different Sniffer format types. Removed old binaries.

--- ParseSnifferData.py
+++ ParseSnifferData.py
@@ -1,9 +1,11 @@
 """
-LKB (c) 2016
+LKB (c) 2016-17
 
 Processing Nivel220 sensor (RS485 interface) output, as collected by Portmon for Windows port sniffer.
 Output:
 Time, X,Y,T
+
+written for python 2.7
 """
 
 #import pandas as pd
@@ -33,22 +35,33 @@ def parserNivel(dataFileName):
   ##################
   for line in open(dataFileName).readlines():
     if line.find('C1N1 X')>0:
-      Val =  line.split(" ")
+      line = re.sub(": ", ":", line) #remove gaps
+      #Val =  line.split(" ") #only one argument allowed
+      Val =  re.split('[\t ]',line)
+      Val = list(filter(None, Val)) #remove empty elements
       iLineCounter=iLineCounter+1
 
       try:
-        time = Val[2:3]
-        Val =  Val[9:12] #get X= Y= T=
-        Val=' '.join(Val).split('.')[:4]  #remove noise at the end
-        Val = re.split(' |:', '.'.join(Val))[1::2] #X Y T
-        Val = time + Val  #time X Y T
-        outString = "{d[0]},{d[1]},{d[2]},{d[3]}\n".format(d=Val)
-        #pdb.set_trace() 
+        time = Val[1]
+        #to get X= Y= T= we need to id it first
+        #base_index=Val.index('.G..C1N1') #to find index of item
+        base_index=[idx for idx, str in enumerate(Val) if 'C1N1' in str][0] #find part of string
+        #I can't just list it from the back as sometimes parser breaks last field
+        Val =  Val[base_index+1:base_index+4] #get X= Y= T=
+        Val[-1] = Val[-1].strip()[:-1]   #remove noise at the end
+        Val = re.split(' |:', ' '.join(Val))[1::2] #X Y T
+        Val.append(time) #X Y T time
+        #pdb.set_trace()
+        outString = "{d[3]},{d[0]},{d[1]},{d[2]}\n".format(d=Val)
+        #time X Y T
+        #
         outString = re.sub("\+", "", outString) #remove +
+
         outFile.write(outString)
 
-      except ValueError:
-        print "Reading error at %s: %s" % (Val[2],ValueError)
+      except: #ValueError:
+        print "Reading error at %s: %s" % (line,ValueError)
+        #pdb.set_trace()
         iErrorCounter=iErrorCounter+1
 
   # finalise outputs
