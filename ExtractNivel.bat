7e471d336a7f0f8c51c5d623f3d7a4d0f1158765
DfAC <DfAC@users.noreply.github.com>
2017-03-27 23:37:45 +0100

    bash updated

--- ExtractNivel.bat
+++ ExtractNivel.bat
@@ -1,14 +1,13 @@
-rem LKB 2014-16 (c)
+rem LKB 2014-17 (c)
 rem extract data from Nivel 220 using port snipping log files
-rem this script looks at Nivel response to catch all responses
 
 @ECHO OFF
 
 md SS
 
 rem first cut headers
-FOR /f %%F IN ('dir /b *.log') DO (
-grep "C1N1 X" %%F | tr -s ' ' | cut -d" " -f1,2,7-9 | cut -d. -f1-4 | tr -s ' ' ',' > %%~nF.csv
+FOR /f %%F IN ('dir /b *.LOG') DO (
+grep "C1N1 X" %%F | tr -s ' ' | cut -f2,7-9 | cut -d. -f1-7 | tr -s '\t :' ',' | cut -d"," -f1,8,10,12 | tr '+' ' ' > %%~nF.csv
 )
 
 mv *.log ./SS
